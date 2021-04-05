46b4d89a807b58ead911640602a9df1870b8970f
William Pereira <william-pereira@hotmail.com>
2021-04-04 23:36:06 -0300

    jsjs

---
+++ js/script.js
@@ -0,0 +1,6 @@
+new SimpleSlide ({
+    slide: 'quote', // nome do atributo data-slide="principal"
+    nav: false, // se deve ou não mostrar a navegação
+    time: 5000, // tempo de transição dos slides
+
+})
