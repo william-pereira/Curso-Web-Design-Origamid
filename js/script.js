2627df9566f3e0b7edf62c8954706aa0873ad75b
William Pereira <william-pereira@hotmail.com>
2021-04-04 23:53:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-new SimpleSlide ({
+if (window.SimpleSlide) {
+  new SimpleSlide ({
     slide: 'quote', // nome do atributo data-slide="principal"
     nav: false, // se deve ou não mostrar a navegação
     time: 5000, // tempo de transição dos slides
 
 })
+new SimpleSlide ({
+  slide: 'portfolio',
+  nav: true, 
+  time: 5000,
+})
+}
